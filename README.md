de6af123f9f16f2f200a8870fc31fbe6535719ac
Dan O'Huiginn <daniel@ohuiginn.net>
2015-07-12 21:13:58 +0200

    Shoehorning in the changing layer-opacity

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Openoil Tanzania
 ===========
 
 With thanks to http://crowdcover.github.io/site/
+
+
+## Merging in different right-panel frames
+
+The original moabi has only 1 right-hand panel, namely a map. We change this to have 3 layers -- custom html content, an iframe for external content, and the map. At each waypoint (i.e. scrolling position), we change the z-index and opacity of each layer
+
+
--- js/app/report-app.js
+++ js/app/report-app.js
@@ -1,4 +1,12 @@
 // report module
+
+var openoil_set_layers = function($this){
+    iframe_opacity = $this.data('oo_iframe-opacity') || 0;
+    html_opacity = $this.data('oo_html_opacity') || 0;
+    $('#openoil_map_overlay_iframe').css('opacity', iframe_opacity);
+    $('#openoil_map_overlay_localcontent').css('opacity', html_opacity);
+}
+
 define(['app/map-app', 'waypoints'], function (moabi, waypoints) {
   $.extend(moabi, {
     initReport: function() {
@@ -24,6 +32,8 @@ define(['app/map-app', 'waypoints'], function (moabi, waypoints) {
       var nav = $this.data('nav'),
           newLayers = $this.data('id'),
           newLayer;
+
+	openoil_set_layers($this);
 
       if(nav){
           moabi.map.setView([nav[0], nav[1]], nav[2]);
